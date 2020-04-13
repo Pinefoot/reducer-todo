d1faf0aec6520c96250affdee9e65936ce87cb22
Pinefoot <jameslcarpino@gmail.com>
2020-04-13 14:35:15 -0700

    Step 1 Complete.

---
+++ todo/src/reducers/Reducers.js
@@ -0,0 +1,16 @@
+import React, {useState, useReducer} from 'react';
+
+
+
+const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+  }
+
+  const reducer = (state, action) => {
+      return state;
+
+  }
+
+  export {initialState, reducer};
